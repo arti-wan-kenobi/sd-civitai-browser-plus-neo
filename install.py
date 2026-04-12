f31a6082fc3adabbe91a3b27ba5bd91168f1ef1e
Artur <arti111182@gmail.com>
2026-04-12 20:30:39 +0200

    Update branding references to "SD CivitAI Browser+ NEO" across files. Adjust gitignore to include `.idea` directory and modernize README for active maintenance.

--- install.py
+++ install.py
@@ -11,7 +11,7 @@ def install_req(check_name, install_name=None):
     if not install_name: install_name = check_name
     if not launch.is_installed(f"{check_name}"):
         print("")
-        launch.run_pip(f"install {install_name}", f"Installing missing requirement \"{check_name}\" for CivitAI Browser")
+        launch.run_pip(f"install {install_name}", f"Installing missing requirement \"{check_name}\" for SD CivitAI Browser+ NEO")
 
 install_req("send2trash", "send2trash==1.8.2")
 install_req("zip_unicode", "ZipUnicode==1.1.1")
